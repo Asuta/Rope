2aa415525adbf2e64d02c6c45bbf3ba17fce962b
TOTHEMOON\youdo <1023571809@qq.com>
2024-08-25 09:15:08 +0800

    不知道是啥  先存上

--- Rope.bat
+++ Rope.bat
@@ -1,3 +1,4 @@
-call venv\Scripts\activate.bat
-python Rope.py 
+@echo off
+call C:\Users\youdo\Anaconda3\Scripts\activate.bat Rope
+C:\Users\youdo\Anaconda3\envs\Rope\python.exe D:\PythonProject\Rope\Rope.py
 pause
